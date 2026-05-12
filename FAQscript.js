b2e9cdcc8646de289595d33659cba439efca40f6
GroupProject351 <adel.lujain2005@gmail.com>
2026-05-12 20:17:48 +0300

    Add files via upload

---
+++ FAQscript.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    const buttons = document.querySelectorAll('.question');
+
+    buttons. forEach(button => {
+        button.addEventListener('click', function() {
+
+            const answer = this.nextElementSibling;
+
+            document.querySelectorAll('.answer').forEach(otherAnswer => {
+                if (otherAnswer !== answer) {
+                    otherAnswer.classList.remove('show');
+                }
+            });
+
+            answer.classList.toggle('show');
+        });
+    });
+});
